29286122e990e9202e2c2a3265b25dda4ab31cb2
Georges <georgeshutschinski@gmail.com>
2024-03-07 09:35:24 -0300

    Criando os componentes para textos

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,14 +1,15 @@
 import Title from '../../components/Title'
+import Paragraph from '../../components/Paragraph'
 
 const About = () => (
   <section>
-    <Title>About me</Title>
-    <p>
+    <Title fontSize={16}>About me</Title>
+    <Paragraph type="principal">
       Eu ea laboris occaecat ea laboris fugiat culpa dolore enim fugiat officia.
       Nulla sit culpa Lorem culpa. Proident nulla enim incididunt voluptate sit
       eu deserunt. Eu id elit ut sunt ut ex. Duis cupidatat consequat sit sit
       Lorem id aliqua Lorem ipsum sint ex.
-    </p>
+    </Paragraph>
   </section>
 )
 
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Title from '../../components/Title'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/schorchi-GH.png" />
-    <Title>Schorchi Hutschinski</Title>
+    <Title fontSize={20}>Schorchi Hutschinski</Title>
   </aside>
 )
 
