0e1729756207a0e6b458890c8ba2096e0a89ab87
Georges <georgeshutschinski@gmail.com>
2024-03-07 08:27:46 -0300

    construindo a estrutura

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,15 @@
+import Title from '../../components/Title'
+
+const About = () => (
+  <section>
+    <Title>About me</Title>
+    <p>
+      Eu ea laboris occaecat ea laboris fugiat culpa dolore enim fugiat officia.
+      Nulla sit culpa Lorem culpa. Proident nulla enim incididunt voluptate sit
+      eu deserunt. Eu id elit ut sunt ut ex. Duis cupidatat consequat sit sit
+      Lorem id aliqua Lorem ipsum sint ex.
+    </p>
+  </section>
+)
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/schorchi-GH.png" />
+    <Title>Schorchi Hutschinski</Title>
+  </aside>
+)
+
+export default Sidebar
