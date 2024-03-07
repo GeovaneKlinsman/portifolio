7a94bc52dafbe7567c5cef9aa607b9e7de0d0ae7
Georges <georgeshutschinski@gmail.com>
2024-03-07 10:06:26 -0300

    finalizando a seção About me

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Paragraph from '../../components/Paragraph'
+import { GithubSection } from './styles'
 
 const About = () => (
   <section>
@@ -10,6 +11,10 @@ const About = () => (
       eu deserunt. Eu id elit ut sunt ut ex. Duis cupidatat consequat sit sit
       Lorem id aliqua Lorem ipsum sint ex.
     </Paragraph>
+    <GithubSection>
+      <img src="https://github-readme-stats.vercel.app/api?username=schorchi-GH&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=schorchi-GH&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSection>
   </section>
 )
 
